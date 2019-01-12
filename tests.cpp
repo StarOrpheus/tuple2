e84fc54b9cb59139800e412498d2e79e092640ec
Zahar Koval <ellesterate@gmail.com>
2019-01-12 17:13:52 +0300

    initial

---
+++ tests.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <functional>
+#include <algorithm>
+#include <random>
+
+#include "gtest/gtest.h"
+#include "tuple2.h"
+
+TEST(basic, constructor)
+{
+    tuple2<std::string, int, double> t;
+    get<1>(t) = 4;
+    get<0>(t) = "sta";
+    assert(get<0>(t) == "sta");
+    assert(get<1>(t) == 4);
+    assert(get<int>(t) == 4);
+    assert(get<std::string>(t) == "sta");
+
+
+    tuple2<std::string, int, double> t2 = t;
+    assert(get<0>(t) == get<0>(t2));
+    assert(get<1>(t) == get<1>(t2));
+    assert(get<2>(t) == get<2>(t2));
+    assert(get<std::string>(t) == get<std::string>(t2));
+    assert(get<int>(t) == get<int>(t2));
+    assert(get<double>(t) == get<double>(t2));
+
+    tuple2<std::string, int, double> t3 = std::move(t2);
+    assert(get<std::string>(t2).empty());
+    assert(!get<std::string>(t3).empty());
+    
+    t2 = std::move(t3);
+    assert(get<std::string>(t3).empty());
+    assert(!get<std::string>(t2).empty());
+
+    tuple2<int, double, int> tt(1, 23.0, 4);
+    assert(get<0>(tt) == 1);
+    assert(get<1>(tt) - 23.0 < 1e-9);
+    assert(get<2>(tt) == 4);
+}
+
+template <typename ...Args>
+using container = tuple2<Args...>;
+
+TEST(correctness, empty)
+{
+    std::tuple<> t;
+}
+
+TEST(correctness, int)
+{
+    const container <int> t1{ 5 };
+    assert(get<0>(t1) == 5);
+    assert(get<int>(t1) == 5);
+}
+
+TEST(correcntess, int_and_string)
+{
+    container<int, std::string> t1{ 5, "hey" };
+    EXPECT_EQ(get<0>(t1), 5);
+    EXPECT_EQ(get<std::string>(t1), "hey");
+}
+
+
+TEST(correcntess, copy_ctor)
+{
+    container<int, long> t1{ 5, 10 };
+    auto t2 = t1;
+    EXPECT_EQ(get<0>(t2), 5);
+    EXPECT_EQ(get<1>(t2), 10);
+}
+
+TEST(correcntess, ce)
+{
+    container<int, std::string, int> t1{ 5, "hey", 6 };
+    EXPECT_EQ(get<0>(t1), 5);
+    EXPECT_EQ(get<2>(t1), 6);
+    EXPECT_EQ(get<std::string>(t1), "hey");
+    get<std::string>(t1).clear();
+    assert(get<std::string>(t1) != "hey");
+}
+
+#include <stdio.h>
+
+GTEST_API_
+int main(int argc, char **argv) {
+    printf("Running main() from gtest_main.cc\n");
+    testing::InitGoogleTest(&argc, argv);
+//  testing::GTEST_FLAG(filter) = "constructors*";
+    return RUN_ALL_TESTS();
+}
---
+++ tuple2.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#include <typeindex>
+
+namespace tuple2_impl_
+{
+    template<typename IndexSequence, typename... Ts>
+    struct tuple2_impl;
+}
+
+template<typename... Ts>
+struct tuple2 final
+    : tuple2_impl_::tuple2_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...>
+{
+    using ImplType = tuple2_impl_::tuple2_impl<std::make_index_sequence<sizeof...(Ts)>, Ts...>;
+    using ImplType::ImplType;
+    tuple2() = default;
+};
+
+template<size_t Ind, typename... Ts>
+decltype(auto) get(tuple2<Ts...>& t)
+{
+    return tuple2_impl_::get<Ind>(t);
+}
+
+template<size_t Ind, typename... Ts>
+decltype(auto) get(tuple2<Ts...> const& t)
+{
+    return tuple2_impl_::get<Ind>(t);
+}
+
+template<typename Ind, typename... Ts>
+decltype(auto) get(tuple2<Ts...>& t)
+{
+    return tuple2_impl_::get<Ind>(t);
+}
+
+template<typename Ind, typename... Ts>
+decltype(auto) get(tuple2<Ts...> const& t)
+{
+    return tuple2_impl_::get<Ind>(t);
+}
+
+namespace tuple2_impl_
+{
+    template <typename T, std::size_t I>
+    struct indexed
+    {
+        using type = T;
+    };
+
+    template<typename T, size_t I>
+    struct indexed_elem_storage : indexed<T, I>
+    {
+        indexed_elem_storage() = default;
+
+        indexed_elem_storage(T const& other) : holder(std::forward<T const&>(other))
+        {}
+
+        indexed_elem_storage(T&& other) : holder(std::forward<T&&>(other))
+        {}
+
+        T& get() { return holder; }
+        T const& get() const { return holder; }
+
+    private:
+        T holder;
+    };
+
+    template <typename Is, typename ...Ts>
+    struct indexer;
+
+    template <std::size_t ...Is, typename ...Ts>
+    struct indexer<std::index_sequence<Is...>, Ts...>
+        : indexed<Ts, Is>...
+    { };
+
+    template <std::size_t I, typename T>
+    static indexed<T, I> select(indexed<T, I>);
+
+    template <std::size_t I, typename ...Ts>
+    using nth_element = typename decltype(select<I>(
+        indexer<std::index_sequence_for<Ts...>, Ts...>{}
+    ))::type;
+
+    template<typename T, typename Head, typename... Ts>
+    struct find_index
+    {
+        constexpr static size_t value = find_index<T, Ts...>::value + 1;
+    };
+
+    template<typename T, typename... Ts>
+    struct find_index<T, T, Ts...>
+    {
+        constexpr static size_t value = 0;
+    };
+
+    template<typename T, typename Head, typename... Ts>
+    struct count_occurences
+    {
+        constexpr static size_t value = count_occurences<T, Ts...>::value;
+    };
+
+    template<typename T, typename... Ts>
+    struct count_occurences<T, T, Ts...>
+    {
+        constexpr static size_t value = 1 + count_occurences<T, Ts...>::value;
+    };
+
+    template<typename T, typename Head>
+    struct count_occurences<T, Head>
+    {
+        constexpr static size_t value = 0;
+    };
+
+    template<typename T>
+    struct count_occurences<T, T>
+    {
+        constexpr static size_t value = 1;
+    };
+
+    // template<typename T, typename... Ts, typename Ok>
+    // struct find_the_only_one_index
+    // {};
+
+    template<typename... Ts, size_t... I>
+    struct tuple2_impl<std::index_sequence<I...>, Ts...> : indexed_elem_storage<Ts, I>...
+    {
+        using Type = tuple2_impl<std::index_sequence<I...>, Ts...>;
+        tuple2_impl() = default;
+
+        tuple2_impl(Type const& other)
+            : indexed_elem_storage<Ts, I>(static_cast<indexed_elem_storage<Ts, I> const&>(other))...
+        {}
+
+        tuple2_impl(Type&& other) noexcept
+            : indexed_elem_storage<Ts, I>(static_cast<indexed_elem_storage<Ts, I>&&>(std::move(other)))...
+        {}
+
+        tuple2_impl(Ts... args) : indexed_elem_storage<Ts, I>(std::forward<Ts>(args))...
+        {}
+
+        Type& operator=(Type const& other) = default;
+        Type& operator=(Type&& other) = default;
+
+        template<size_t Ind>
+        friend decltype(auto) get(tuple2<Ts...>& t)
+        {
+            using elem_type = nth_element<Ind, Ts...>;
+            return static_cast<indexed_elem_storage<elem_type, Ind>&>(t).get();
+        }
+
+        template<size_t Ind>
+        friend decltype(auto) get(tuple2<Ts...> const& t)
+        {
+            using elem_type = nth_element<Ind, Ts...>;
+            return static_cast<indexed_elem_storage<elem_type, Ind> const&>(t).get();
+        }
+
+        template<typename T,
+            typename T2 = typename std::enable_if<count_occurences<T, Ts...>::value == 1>::type,
+            size_t I = find_index<T, Ts...>::value>
+            friend T& get(tuple2<Ts...>& t)
+        {
+            using elem_type = nth_element<I, Ts...>;
+            return static_cast<indexed_elem_storage<elem_type, I> &>(t).get();
+        }
+
+        template<typename T,
+            typename T2 = typename std::enable_if<count_occurences<T, Ts...>::value == 1>::type,
+            size_t I = find_index<T, Ts...>::value>
+            friend T const& get(tuple2<Ts...> const& t)
+        {
+            using elem_type = nth_element<I, Ts...>;
+            return static_cast<indexed_elem_storage<elem_type, I> const&>(t).get();
+        }
+    };
+}
